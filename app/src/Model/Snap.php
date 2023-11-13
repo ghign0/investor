84f33ad8ff59efde8e94fe0779e1e3e54c4f0484
ghign0 <ghinassi.marco@gmail.com>
2023-11-13 11:57:22 +0100

    update snap

--- app/src/Model/Snap.php
+++ app/src/Model/Snap.php
@@ -2,20 +2,35 @@
 
 namespace App\Model;
 
+use App\Model\ValueObject\Asset;
+use App\Repository\SnapRepository;
+use Doctrine\DBAL\Types\Types;
+use Symfony\Bridge\Doctrine\Types\UuidType;
 use Symfony\Component\Uid\Uuid;
+use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\Entity(repositoryClass: SnapRepository::class)]
 class Snap
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator' )]
+    #[ORM\Column( type: UuidType::NAME, length: 255)]
     private Uuid $id;
 
-    private Asset $asset;
+    #[ORM\ManyToOne(targetEntity: Wallet::class)]
+    private Wallet $asset;
 
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
     private float $invested;
 
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
     private float $notInvested;
 
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
     private float $value;
 
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
     private float $gain;
 
 }
